2ed2571ce3ea1ebdc778d0c5a1385e6fe47a5c68
Joshua Cooper <josh@joshdotc.com>
2021-10-12 20:39:43 -0400

    Basic `FruitModel` training and prediction methods

--- model/fruit.py
+++ model/fruit.py
@@ -1,30 +1,34 @@
 import torch
+import torch.nn.functional
+import torch.optim as opt
 import torchvision
-import torchvision.models.detection as models
+from torch.utils.data import Dataset
 from torchvision.datasets import ImageFolder
 
 
 class FruitModel:
-    def __init__(self, model: "torch.Module" = None, threshold=0.6):
+    def __init__(self, labels: "list[str]", model: "torch.nn.Module" = None, threshold=0.6):
+        """
+        Wrapper class for PyTorch model prediction and training.
+
+        :param model: PyTorch module to predict/train against.
+        :type model: torch.Module
+
+        :param threshold: Value on range (0.0, 1.0] where only predictions which meet the confidence threshold
+        are considered valid. Any predictions whose confidence scores do not exceed the threshold are
+        deemed invalid and are discarded.
+        :type threshold: float
+
+        :param labels: Optional list of indexed label strings representing the names for each classification.
+        :type labels: list[str]
+        """
+        self._labels = labels
+
         if model is None:
-            # Set pretained=False once we have our own training pipeline
-            model = models.retinanet_resnet50_fpn(pretrained=True)
-        self.__model = model
-        # TODO: replace default labels with our own
-        self.__labels = [
-            '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
-            'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
-            'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-            'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
-            'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-            'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
-            'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
-            'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-            'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
-            'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
-            'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
-            'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
-        ]
+            model = torchvision.models.resnet50(pretrained=False)
+            model.fc = torch.nn.Linear(model.fc.in_features, len(self._labels))
+        self._model = model.to("cpu")
+
         self.threshold = float(threshold)
 
     @classmethod
@@ -39,29 +43,88 @@ class FruitModel:
         return cls.get_transform()(data)
 
     def predict(self, img_data: "torch.Tensor") -> "list[tuple[str, float]]":
-        img_data = torch.unsqueeze(img_data, 0)
-        m = self.__model.eval()
+        m = self._model.eval()
         with torch.no_grad():
-            predictions = m(img_data)[0]
+            predictions = m(img_data)
+        probabilities = torch.nn.functional.softmax(predictions, dim=1).squeeze(0)
         return [
-            (self.__labels[label], score.item())
-            for label, score in zip(predictions["labels"], predictions["scores"])
-            if score.item() >= self.threshold
+            (self._labels[label_id], score.item())
+            for label_id, score in enumerate(probabilities)
+            if score.item() > self.threshold
         ]
 
+    @classmethod
+    def from_file(cls, path: "str", labels: "list[str]", threshold=0.6) -> "FruitModel":
+        """
+        Import a model from a .pth file.
 
-class FruitTrainingModel:
-    def __init__(self, model: "FruitModel"):
-        self.__model = model
+        :param path: Absolute location of file to import.
+        :type path: str
 
-    def detach(self):
-        model = self.__model
-        self.__model = None
-        return model
+        :param threshold: Value on range (0.0, 1.0] where only predictions which meet the confidence threshold
+        are considered valid. Any predictions whose confidence scores do not exceed the threshold are
+        deemed invalid and are discarded.
+        :type threshold: float
 
-    def run_epoch(self):
-        pass
+        :param labels: Optional list of indexed label strings representing the names for each classification.
+        :type labels: list[str]
+
+        :return: Model instantiated from the indicated .pth file.
+        """
+        pth_model = torchvision.models.resnet50(pretrained=False, num_classes=len(labels))
+        pth_model.load_state_dict(torch.load(path))
+        return FruitModel(model=pth_model, threshold=threshold, labels=labels)
+
+    def export(self, path: "str"):
+        """
+        Save the given model as a .pth file.
+
+        :param path: Absolute location of file to export to.
+        :type path: str
+        """
+        torch.save(self._model.state_dict(), path)
+
+
+class FruitTrainingModel(FruitModel):
+    def __init__(self, model: "torch.nn.Module" = None, threshold=0.6, labels: "list[str]" = None, optimizer=None, loss=None):
+        super(FruitTrainingModel, self).__init__(labels=labels, model=model, threshold=threshold)
+        self._model.train()
+
+        if optimizer is None:
+            optimizer = opt.SGD(self._model.parameters(), lr=0.001)
+        self.__optimizer = optimizer
+
+        if loss is None:
+            loss = torch.nn.CrossEntropyLoss()
+        self.__loss = loss
+
+    def detach(self) -> "FruitModel":
+        """
+        Generate a new trained model suitable for predictions from the current model.
+        All training methods will be disabled in the generated model, and is optimized for inference.
+
+        :return: Non-trainable copy of the training model.
+        """
+        return FruitModel(labels=self._labels, model=self._model.eval(), threshold=self.threshold)
+
+    def run_epoch(self, data: "Dataset") -> "float":
+        """
+        Performs a single training iteration on the model with the given PyTorch dataset.
+        The labels provided by the dataset MUST match the instantiated model.
+
+        :param data: PyTorch dataset with labels matching the model.
+        :type data: torch.utils.data.Dataset
+
+        :return: Float representing loss value of the iteration.
+        """
+        values, labels = data
+        self.__optimizer.zero_grad()
+        result = self._model(values)
+        loss = self.__loss(result, labels)
+        loss.backward()
+        self.__optimizer.step()
+        return loss.item()
 
     @classmethod
-    def get_dataset(cls, root: "str"):
+    def get_dataset(cls, root: "str") -> "ImageFolder":
         return ImageFolder(root=root, transform=FruitModel.get_transform())
