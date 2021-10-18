1e9e41701bdaea291d7c5f19fc149c4cae1eef1f
Nicholas <nharris4@unca.edu>
2021-10-18 18:24:37 -0400

    Revamped HTML. Integrated model. Added model.pth.

--- web/app.py
+++ web/app.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python
+
 import os
 from flask import Flask, render_template, request
-import model.fruit
 import torch
 import torch.utils.data
 import torch.nn as nn
@@ -8,11 +9,25 @@ import torch.nn.functional as func
 import torchvision
 import numpy as np
 from PIL import Image
+import os
+import sys
+sys.path.insert(0, '/mnt/c/Users/Nicholas/PycharmProjects/csci338-fruit-ai/')
+
+import model.fruit
 
 app = Flask(__name__)
 UPLOAD_FOLDER = './static/images'
 ALLOWED_EXTENSION = {'jpg', 'jpeg', 'png'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+modelPath = r'/mnt/c/Users/Nicholas/Pictures/archive/fruits-360_dataset/fruits-360/AI_fruit/model.pth'
+trainingPath = r'/mnt/c/Users/Nicholas/Pictures/archive/fruits-360_dataset/fruits-360/AI_fruit/training'
+classes = ['Apple Red 1', 'Apple Red 2', 'Apple Red 3', 'Apricot', 'Banana', 'Blueberry', 'Cantaloupe 1',
+           'Cantaloupe 2', 'Cherry 1', 'Cherry 2', 'Cocos', 'Guava', 'Kiwi', 'Lemon', 'Limes', 'Mango', 'Orange',
+           'Papaya', 'Peach', 'Peach 2', 'Peach Flat', 'Pear', 'Pear 2', 'Pineapple', 'Raspberry', 'Strawberry',
+           'Strawberry Wedge', 'Tomato 1', 'Tomato 2', 'Tomato 3', 'Watermelon']
+
+mx = model.fruit.FruitModel.from_file(labels=classes, path=modelPath)
 
 
 # Main page for project
@@ -35,7 +50,13 @@ def get_results():
         # save file in /static/images folder
         fullName = os.path.join(app.config['UPLOAD_FOLDER'], newName)
         file.save(fullName)
-    return render_template('results.html', imgpath=newName)
+        img = Image.open(fullName)
+        # save results
+        results = mx.predict(img)
+        # extract fruit name and confidence from results
+        fruitResults = str(list(results.keys()))[2:-2].lower()
+        confidence = "{:.2f}".format(float(str(list(results.values()))[2:-2]) * 100)
+    return render_template('results.html', imgpath=newName, aiFruit=fruitResults, aiCon=confidence)
 
 
 if __name__ == '__main__':
