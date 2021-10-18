fa08e07e506a0e7321fa04b9fef0af791917966d
Joshua Cooper <josh@joshdotc.com>
2021-10-18 19:17:55 -0400

    Fix file paths to be universal (os.path.join())

--- web/app.py
+++ web/app.py
@@ -10,18 +10,28 @@ import torchvision
 import numpy as np
 from PIL import Image
 import os
+import errno
 import sys
-sys.path.insert(0, '/mnt/c/Users/Nicholas/PycharmProjects/csci338-fruit-ai/')
 
+rootDir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+sys.path.insert(0, rootDir)
 import model.fruit
 
 app = Flask(__name__)
-UPLOAD_FOLDER = './static/images'
+UPLOAD_FOLDER = os.path.join(rootDir, "web", "static", "images")
+
+# Attempt to create the folder if it doesn't already exist
+try:
+    os.mkdir(UPLOAD_FOLDER)
+except OSError as err:
+    if err.errno != errno.EEXIST:
+        raise
+
 ALLOWED_EXTENSION = {'jpg', 'jpeg', 'png'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-modelPath = r'/mnt/c/Users/Nicholas/Pictures/archive/fruits-360_dataset/fruits-360/AI_fruit/model.pth'
-trainingPath = r'/mnt/c/Users/Nicholas/Pictures/archive/fruits-360_dataset/fruits-360/AI_fruit/training'
+modelPath = os.path.join(rootDir, "model.pth")
+trainingPath = os.path.join(rootDir, "training")
 classes = ['Apple Red 1', 'Apple Red 2', 'Apple Red 3', 'Apricot', 'Banana', 'Blueberry', 'Cantaloupe 1',
            'Cantaloupe 2', 'Cherry 1', 'Cherry 2', 'Cocos', 'Guava', 'Kiwi', 'Lemon', 'Limes', 'Mango', 'Orange',
            'Papaya', 'Peach', 'Peach 2', 'Peach Flat', 'Pear', 'Pear 2', 'Pineapple', 'Raspberry', 'Strawberry',
